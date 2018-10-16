952a3f7d092bf1029d1d518cf16fd70ecb452d1a
zhangwenxuan <1065344157@qq.com>
2018-10-16 16:25:03 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        String a="false";
+        boolean b=true;
+        b=Boolean.valueOf(a).booleanValue();
+        System.out.println(b);
+    }
+}
